273dce733fd77dad90b78f1cc2e481e32472bfe2
RoiiiHan <roihanntauhidd@gmail.com>
2025-10-24 19:49:39 +0700

    done html tailwind

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-// namaPengunjung()
+//  namaPengunjung()
 
-function namaPengunjung(){
-    let nama = prompt("Masukkan Nama Anda : ")
+function namaPengunjung() {
+  let nama = prompt("Masukkan Nama Anda : ");
 
-    document.getElementById("nama").innerHTML = `HI ${nama}, `
-
+  document.getElementById("nama").innerHTML = `HI ${nama}, `;
 }
+
+function resultForm(event) {
+  event.preventDefault();
+  const name = document.getElementById("name-input").value;
+  const tempatL = document.getElementById("tempatLahir").value;
+  const jk = document.querySelector("input[name='jk']:checked");
+  const message = document.getElementById("message-input").value;
+
+  if (jk.checked) {
+    documentgetElementById;
+  }
+
+  document.getElementById("nama-result").innerHTML = `: ${name}`;
+  document.getElementById("tempatL").innerHTML = `: ${tempatL}`;
+  document.getElementById("jk").innerHTML = `: ${jk}`;
+  document.getElementById("message").innerHTML = `: ${message}`;
+}
+
+resultForm();
